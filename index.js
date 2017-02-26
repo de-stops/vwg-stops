245a842c2116cbc9760e0b9fc221ab0f222b587d
highsource <aleksei.valikov@gmail.com>
2017-02-26 12:22:09 +0100

    Initial commit

---
+++ index.js
@@ -0,0 +1,5 @@
+const exportStops = require("hafas-export-stops-by-coordinates");
+
+const ENDPOINT_BASE_URL_TEMPLATE = "http://routenplaner.bus-bahn-thueringen.de/bin/query.exe/dny?performLocating=2&tpl=stop2json&look_stopclass=2147483647&look_minx={minx}&look_miny={miny}&look_maxx={maxx}&look_maxy={maxy}";
+
+exportStops(ENDPOINT_BASE_URL_TEMPLATE, 5, 47, 15, 56, null, [51, 54, 80, 81, 82, 83, 84, 85, 86, 87, 88]);
